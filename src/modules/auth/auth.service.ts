6a36c7ab5e9812ff9cded54f4806d6b17724c757
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-28 01:18:00 +0600

    auth password match work done

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,22 @@
-const loginUser =()=>{
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import { ILogInUser } from "./auth.interface"
 
+const loginUser =async (payload: ILogInUser)=>{
+
+    const { email, password } = payload;
+
+    const user =await prisma.user.findUniqueOrThrow({
+        where: { email }
+    })
+
+    const isPassWordMatched =await bcrypt.compare(password, user.password);
+
+    if (!isPassWordMatched) {
+        throw new Error("Invalid credentials");
+    }
+
+    return user;
 }
 
 export const authService = {
