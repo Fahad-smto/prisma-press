e05f681982299327588b9a6b53a2640f5685f9f1
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-27 23:40:44 +0600

    catchAsync added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -23,7 +23,9 @@ const sendResponse = <T>(res:Response,data:TResponseData<T>) => {
     res.status(data.statusCode).json({
         success: data.success,
         statusCode: data.statusCode,
-       
+        message: data.message,
+        data: data.data,
+        meta: data.meta
     });
 
 }
