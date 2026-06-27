369e43ab0272440d4c6955ef5e836f0ef81643be
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-27 22:50:39 +0600

    catchAsync added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,33 +1,70 @@
-import { Request, Response } from "express";
+import { UserCreateArgs } from './../../../generated/prisma/models/User';
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 
-const registerUser = async (req: Request, res: Response) => {
-    try {
-        const payload = req.body;
 
-        const user = await userService.registerUserIntoDB(payload);
+const catchAsync = (fn: RequestHandler) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await fn(req, res, next);
+        } catch (error) {
+            next(error)
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+                success: false,
+                statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+                message: "Failed to register user",
+                error: (error as Error).message
+            })
 
-        res.status(httpStatus.CREATED).json({
-            success: true,
-            statusCode: httpStatus.CREATED,
-            message: "User registered successfully",
-            data: {
-                user
-            }
-        });
-    } catch (error) {
-        console.log(error);
-
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            success: false,
-            statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-            message: "Failed to register user",
-            error: (error as Error).message
-        })
-
+        }
     }
 }
+
+
+
+// const registerUser = async (req: Request, res: Response) => {
+//     try {
+//         const payload = req.body;
+
+//         const user = await userService.registerUserIntoDB(payload);
+
+//         res.status(httpStatus.CREATED).json({
+//             success: true,
+//             statusCode: httpStatus.CREATED,
+//             message: "User registered successfully",
+//             data: {
+//                 user
+//             }
+//         });
+//     } catch (error) {
+//         console.log(error);
+
+//         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//             success: false,
+//             statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//             message: "Failed to register user",
+//             error: (error as Error).message
+//         })
+
+//     }
+// }
+
+
+const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const payload =req.body;
+    const user = await userService.registerUserIntoDB(payload);
+
+    res.status(httpStatus.CREATED).json({
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message:'user registered successfully',
+        data: {
+            user
+        }
+    })
+})
+
 
 export const userController = {
     registerUser
