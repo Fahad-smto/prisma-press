c5bcb2ae08ca08ee5c441799d073037d6fc9e290
Fahad-smto <simanto.smto.prog@gmail.com>
2026-06-29 11:16:21 +0600

    middleware implemantation complete

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -5,81 +5,88 @@ import { Role } from "../../../generated/prisma/client";
 import jwt from "jsonwebtoken";
 import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
+import { jwtUtils } from "../../utils/jwt";
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
-declare global {
-    namespace Express {
-        interface Request {
-            user?: {
-                email: string;
-                name: string;
-                id: string;
-                role: Role;
-            };
-        }
-    }
-}
+// declare global {
+//     namespace Express {
+//         interface Request {
+//             user?: {
+//                 email: string;
+//                 name: string;
+//                 id: string;
+//                 role: Role;
+//             };
+//         }
+//     }
+// }
 
 router.post("/register", userController.registerUser);
 
 
-const auth = ()=>{
-    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+// const auth = () => {
+//     return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-        const token = req.cookies.accessToken||req.headers.authorization?.startsWith('Bearer')?req.headers.authorization?.split(' ')[1]:req.headers.authorization;
+//         const token = req.cookies.accessToken || req.headers.authorization?.startsWith('Bearer') ? req.headers.authorization?.split(' ')[1] : req.headers.authorization;
 
-        if (!token) {
-            return res.status(httpStatus.UNAUTHORIZED).json({ error: "No access token provided" });
-        }
+//         if (!token) {
+//             return res.status(httpStatus.UNAUTHORIZED).json({ error: "No access token provided" });
+//         }
 
-        const verifiedToken = jwt.verify(token, config.jwt_access_secret as string) as jwt.JwtPayload;
+//         const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret as string) as jwt.JwtPayload;
 
-    })
-}
+//     })
+// }
 
 
-router.get(
-    "/me",
-    (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const { accessToken } = req.cookies;
+router.get("/me",auth(Role.ADMIN, Role.USER, Role.AUTHOR),userController.getUserProfile);
+    // (req: Request, res: Response, next: NextFunction) => {
+    //     try {
+    //         const { accessToken } = req.cookies;
 
-            if (!accessToken) {
-                return res
-                    .status(httpStatus.UNAUTHORIZED)
-                    .json({ error: "No access token provided" });
-            }
+    //         if (!accessToken) {
+    //             return res
+    //                 .status(httpStatus.UNAUTHORIZED)
+    //                 .json({ error: "No access token provided" });
+    //         }
 
-            const verifiedToken = jwt.verify(
-                accessToken,
-                config.jwt_access_secret as string
-            ) as jwt.JwtPayload;
+    //         const verifiedToken = jwt.verify(
+    //             accessToken,
+    //             config.jwt_access_secret as string
+    //         ) as jwt.JwtPayload;
 
-            const { email, name, id, role } = verifiedToken as {
-                email: string;
-                name: string;
-                id: string;
-                role: Role;
-            };
 
-            const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+    //         if (!verifiedToken.success) {
+    //             throw new Error(verifiedToken.error || "Invalid token");
+    //         }
 
-            if (!role || !requiredRoles.includes(role)) {
-                return res
-                    .status(httpStatus.FORBIDDEN)
-                    .json({ error: "Access denied" });
-            }
 
-            req.user = { email, name, id, role };
-            next();
-        } catch (error) {
-            return res
-                .status(httpStatus.UNAUTHORIZED)
-                .json({ error: "Invalid or expired token" });
-        }
-    },
-    userController.getUserProfile
-);
+    //         const { email, name, id, role } = verifiedToken as {
+    //             email: string;
+    //             name: string;
+    //             id: string;
+    //             role: Role;
+    //         };
+
+    //         const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+
+    //         if (!role || !requiredRoles.includes(role)) {
+    //             return res
+    //                 .status(httpStatus.FORBIDDEN)
+    //                 .json({ error: "Access denied" });
+    //         }
+
+    //         req.user = { email, name, id, role };
+    //         next();
+    //     } catch (error) {
+    //         return res
+    //             .status(httpStatus.UNAUTHORIZED)
+    //             .json({ error: "Invalid or expired token" });
+    //     }
+    // },
+    // userController.getUserProfile
+
 
 export const userRoutes = router;
