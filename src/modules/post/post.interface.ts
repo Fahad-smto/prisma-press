40a249405c3e26ec613335aed22b546e45164cf7
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-01 22:05:55 +0600

    get post by id added on this transaction

--- src/modules/post/post.interface.ts
+++ src/modules/post/post.interface.ts
@@ -1,14 +1,6 @@
 import { postStatus } from "../../../generated/prisma/enums";
 
-export interface ICreatePostPayload {
-    title : string;
-    content : string;
-    thumbnail ?: string;
-    isFeatured ?: boolean;
-    isPremium ?: boolean
-    status ?: postStatus
-    tags : string[]
-}
+
 
 export interface IUpdatePostPayload{
     title?: string;
