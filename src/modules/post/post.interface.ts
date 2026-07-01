2724594fb5400168c7484a59b5b4758f62bf8182
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-01 22:06:45 +0600

    get post by id added on this transaction

--- src/modules/post/post.interface.ts
+++ src/modules/post/post.interface.ts
@@ -10,14 +10,7 @@ export interface ICreatePostPayload {
     tags : string[]
 }
 
-export interface IUpdatePostPayload{
-    title?: string;
-    content?: string;
-    thumbnail?: string;
-    isFeatured?: boolean;
-    status?: postStatus
-    tags?: string[]
-}
+
 
 // export interface IPostQuery extends PostWhereInput {
 //     //post model er fields
