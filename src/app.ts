db749838509fc76fb839b05689c8fff46ffdfa71
Fahad-smto <simanto.smto.prog@gmail.com>
2026-07-01 22:03:41 +0600

    get post by id added on this transaction

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,9 @@ app.use(express.urlencoded({ extended : true }));
 app.use(cookieParser())
 
 
+app.get("/",(req : Request, res : Response) => {
+    res.send("Hello, World!");
+});
 
 // app.post()
 
